ea83202b72b581ff82522381f340d4d511501b98
陈强 <johnnychenq@cathay-ins.com.cn>
2017-11-22 22:50:50 +0800

    bugfix

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const proxy = require('http-proxy-middleware');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const cwd = process.cwd();
 
 
-module.exports = function () {
-    const port = 80;
 
+module.exports = function (port = 80, dist = './dist') {
     const app = express();
-    
-    
+
     app.use(bodyParser.json({ type: 'application/json' }))
-    app.use(express.static(path.join(__dirname, './dist')));
-    
-    
+    app.use(express.static(path.join(cwd, dist)));
+
+
     app.use(proxy(['**', '**/*.htm', '**/*.do', '**/*.json', '**/*.jsp'], {
         target: 'http://localhost:8001',
         changeOrigin: false
